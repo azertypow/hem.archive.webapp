0abdf7f400755bea6465f48aa6450a77046c9ba9
azertypow <azertypow@gmail.com>
2023-10-16 18:17:45 +0200

    feat: use origin content api

--- global/getDataFromHemApi.ts
+++ global/getDataFromHemApi.ts
@@ -1,7 +1,7 @@
 import {IHemApi_allProjectInfo, IHemApi_communaute, IHemApi_projectDetails} from "~/global/hemApi"
 
-// export const hemApiBaseUrl = 'https://hemadmin.sdrvl.ch/webapp/api/v1'
-export const hemApiBaseUrl = 'http://localhost:8000/webapp/api/v1'
+export const hemApiBaseUrl = 'https://hemadmin.sdrvl.ch/webapp/api/v1'
+// export const hemApiBaseUrl = 'http://localhost:8000/webapp/api/v1'
 
 export async function getProjectsData() {
   const response = await fetch(`${hemApiBaseUrl}/projects`)
