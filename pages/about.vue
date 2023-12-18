37a93a5bb87f2f835d158fcac226c6c9a3d02cef
azertypow <azertypow@gmail.com>
2023-12-18 12:25:18 +0100

    feat: init about template

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <section
+        class="v-about"
+    >
+    </section>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+    message?: string
+}>()
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-about {
+}
+</style>
