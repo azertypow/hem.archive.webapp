3b25d0de4454d8a64a30ff9467236c2beb6f85ea
azertypow <azertypow@gmail.com>
2024-01-29 23:06:15 +0100

    feat: about page template

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,96 @@
 <template>
-    <section
-        class="v-about"
-    >
+    <section class="v-about-index" >
+        <template
+            v-if="communaute === null"
+        >
+            <div
+                class="v-about-index__header"
+            >
+                <div
+                    class="fp-grid-coll-container fp-grid-coll-container--center v-about-index__header__box"
+                >
+                    <div
+                        class="fp-grid-coll-16-24 hem-rm-margins fp-grid-row-container fp-grid-row-container--center fp-grid-row-container--align-center"
+                    >
+                        <div class="hem-loader" ></div>
+                    </div>
+                </div>
+            </div>
+        </template>
+
+        <template
+            v-else-if="errorMessage"
+        >
+            <h1
+                style="color: white"
+            >
+                {{errorMessage}}
+            </h1>
+        </template>
+        <template
+            v-else
+        >
+            <div
+                class="v-about-index__header"
+            >
+                <div
+                    class="fp-grid-coll-container fp-grid-coll-container--center v-about-index__header__box"
+                >
+                    <div
+                        class="fp-grid-coll-16-24 hem-rm-margins fp-grid-row-container fp-grid-row-container--center"
+                    >
+                        <h1>
+                            À Propos
+                        </h1>
+                    </div>
+                </div>
+
+            </div>
+
+            <div
+                class="fp-grid-coll-container fp-grid-coll-container--center"
+            >
+                <div
+                    class="fp-grid-coll-16-24 v-about-index__form"
+                >
+                    <div class="hem-form">
+                        <input type="text" placeholder="Recherche">
+                        <button>recherche</button>
+                    </div>
+                    <div class="hem-form">
+                        <select>
+                            <option selected disabled>Ordre</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+
+            <div
+                class="fp-grid-coll-container fp-grid-coll-container--center"
+            >
+                <div
+                    class="fp-grid-coll-16-24 v-about-index__person-box"
+                >
+                    <nuxt-link
+                        :href="`/communaute/${person.uid}`"
+                        class="v-about-index__person-box__item hem-rm-margins"
+                        v-for="person of communaute.pages"
+                    >
+                        <h3
+                            class="v-about-index__person-box__item__user"
+                        >
+                            {{person.firstname}} <span style="text-transform: uppercase;" >{{person.name}}</span>
+                        </h3>
+                        <h4
+                            class="v-about-index__person-box__item__status"
+                        >
+                            {{person.job}}
+                        </h4>
+                    </nuxt-link>
+                </div>
+            </div>
+        </template>
     </section>
 </template>
 
@@ -9,11 +98,31 @@
 
 
 
-<script setup lang="ts">
+<script lang="ts" setup>
+// defineProps<{
+// }>()
 
-const props = defineProps<{
-    message?: string
-}>()
+import {Ref, UnwrapRef} from "vue";
+import {IHemApi_bloks, IHemApi_bloks_image, IHemApi_communaute} from "~/global/hemApi";
+import {getCommunityData, getProjectDataByUdi} from "~/global/getDataFromHemApi";
+
+const communaute: Ref<UnwrapRef<null | IHemApi_communaute>> = ref(null)
+const errorMessage: Ref<UnwrapRef<null | string>> = ref(null)
+
+onMounted(() => {
+    loadCommunauteDataFromHEMAPI()
+})
+
+async function loadCommunauteDataFromHEMAPI() {
+    const communauteData = await getCommunityData()
+
+    window.setTimeout(() => {
+        if('error' in communauteData) errorMessage.value = communauteData.error
+        else communaute.value = communauteData
+    }, 2_000)
+
+}
+
 </script>
 
 
@@ -21,6 +130,48 @@ const props = defineProps<{
 
 
 <style lang="scss" scoped >
-.v-about {
+.v-about-index {
+    background: white;
+}
+
+.v-about-index__form {
+    display: flex;
+    flex-direction: column;
+    gap: var(--gutter-xl);
+    margin-top: 8rem;
+}
+
+.v-about-index__header {
+    margin-top: var(--nav-height);
+    background-color: #0e264e;
+    color: white;
+    height: 30rem;
+}
+
+.v-about-index__header__box {
+    height: 100%;
+}
+
+.v-about-index__person-box {
+    margin-top: 8rem;
+}
+
+.v-about-index__person-box__item {
+    display: block;
+    border-top: solid 2px black;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    color: black;
+    text-decoration: none;
+}
+
+.v-about-index__person-box__item__user {
+    margin: 0;
+}
+
+.v-about-index__person-box__item__status {
+    margin-top: 0;
+    margin-bottom: 0;
 }
 </style>
+
