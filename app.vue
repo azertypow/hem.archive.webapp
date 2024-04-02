896b6d8327ee4380f5fd08b14ba7cd6852046637
azertypow <azertypow@gmail.com>
2024-04-02 15:52:34 +0200

    chore: clean

--- app.vue
+++ app.vue
@@ -88,7 +88,6 @@ import {getTagsList} from "~/global/getDataFromHemApi";
 
 onMounted(async () => {
   const tagsList = await getTagsList()
-    console.log(tagsList)
   useAppStateStore().tag_themeList = tagsList.listTheme
   useAppStateStore().tag_axesList = tagsList.listAxes
 })
