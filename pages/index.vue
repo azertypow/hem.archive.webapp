4a3dc1df10c637eb0d92b16c82ec7fee83ed27fe
azertypow <nico@villa1203.ch>
2025-10-15 16:02:23 +0200

    feat: Refactor grid layout for large screens

--- pages/index.vue
+++ pages/index.vue
@@ -131,12 +131,10 @@
 }
 
 .v-index__container {
-  /** START todo: demo pour guillaume pour la grille large ici */
   @media (min-width: scss-var.$breakpoint-xl) {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(35rem, 1fr));
   }
-  /** END todo: demo pour guillaume pour la grille large ici */
 }
 
 .v-index__container__items {
@@ -165,16 +163,12 @@
     }
 
     @media (min-width: scss-var.$breakpoint-xl) {
-      width: calc( 100% / 24 * 6 );
-
-      /** END todo: demo pour guillaume pour la grille large ici */
       width: calc( 100% / 24 * 24 );
 
       &.v-index__items--6,
       &.v-index__items--7 {
         width: calc( 100% / 24 * 24 );
       }
-      /** START todo: demo pour guillaume pour la grille large ici */
     }
 }
 
