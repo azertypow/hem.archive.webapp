53ec542f275931807b8214b6786cb838646c7570
azertypow <nico@villa1203.ch>
2026-02-04 10:21:54 +0100

    feat: sort alpha remove l' d' an *

--- global/projectSort.ts
+++ global/projectSort.ts
@@ -1,7 +1,11 @@
 import {IHemApi_projectInfo} from "~/global/hemApi";
 
 function removeLeadingArticles(title: string): string {
-  return title.replace(/^(le|la|un|une|a|an|the|l'|les)\s+/i, '').trim()
+  const titleWithoutAsterisks_WithoutApostrophes =
+    title
+      .replace(/\*/g, '')
+      .replace(/l’|l'|d'|d’/gi, '')
+  return titleWithoutAsterisks_WithoutApostrophes.trim().replace(/^(le|la|un|une|a|an|the|les)\s+/i, '').trim()
 }
 
 export function projectsSort(projects: IHemApi_projectInfo[]): IHemApi_projectInfo[] {
@@ -9,6 +13,10 @@ export function projectsSort(projects: IHemApi_projectInfo[]): IHemApi_projectIn
   return [...projects].sort((a, b) => {
     const titleA = removeLeadingArticles(a.title)
     const titleB = removeLeadingArticles(b.title)
+
+    console.log('a: ', titleA)
+    console.log('b: ', titleB)
+
     return titleA.localeCompare(titleB, ['fr', 'en',], {ignorePunctuation: true, sensitivity: 'base'})
   })
 }
