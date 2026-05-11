c464a7a69984e21af66d80774523f596c7124474
azertypow <nico@villa1203.ch>
2026-05-11 23:39:07 +0200

    feat: add hyphen for project title

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -12,7 +12,10 @@
                 <div
                     class="v-app-header__left"
                 >
-                    <h1 class="" v-html="italicMarkdownToHtml(title)" ></h1>
+                    <h1 class=""
+                        v-html="italicMarkdownToHtml(title)"
+                        style="-webkit-hyphens: auto;-ms-hyphens: auto;hyphens: auto;"
+                    />
                     <div>
                         <div class="v-app-header__title">
                             <h5>Responsable<template v-if="(responsables?.length || 0) > 1">s</template></h5>
