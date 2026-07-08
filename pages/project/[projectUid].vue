a36e2434ad0e6961c8bf6b9e25ef948867962e3a
azertypow <nico@villa1203.ch>
2026-07-08 11:40:16 +0200

    fix: video gallery multi platform support

--- pages/project/[projectUid].vue
+++ pages/project/[projectUid].vue
@@ -309,13 +309,40 @@
                         <div class="v--project-uid__content__video-gallery__scroll-wrap">
                           <template v-for="video of projectItem.content.video_list">
                             <div  class="v--project-uid__content__video-gallery__container">
-                                  <iframe width="1280"
-                                          height="800"
-                                          :src="`https://www.youtube.com/embed/${extractVideoID(video.url)}`"
-                                          frameborder="0"
-                                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                          allowfullscreen
-                                  />
+                              <div
+                                v-if="videoPlatformMatch(video.url) === null"
+                                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: rgba(240,240,240)"
+                              >Erreur avec l'URL: {{video.url}}</div>
+                              <iframe
+                                v-else-if="videoPlatformMatch(video.url)?.videoPlatform === 'switch'"
+                                class="switch-player"
+                                :src="videoPlatformMatch(video.url)?.src" width="1280"
+                                height="720"
+                                frameborder="0"
+                                allow="fullscreen"
+                              />
+                              <iframe
+                                v-else-if="videoPlatformMatch(video.url)?.videoPlatform === 'vimeo'"
+                                class="vimeo-player"
+                                :src="videoPlatformMatch(video.url)?.src"
+                                width="1280"
+                                height="720"
+                                frameborder="0"
+                                referrerpolicy="strict-origin-when-cross-origin"
+                                allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+                                allowfullscreen
+                              />
+                              <iframe
+                                v-else-if="videoPlatformMatch(video.url)?.videoPlatform === 'youtube'"
+                                width="1280"
+                                height="720"
+                                :src="videoPlatformMatch(video.url)?.src"
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerpolicy="strict-origin-when-cross-origin"
+                                allowfullscreen
+                              />
                             </div>
                           </template>
                         </div>
